bb9a3346588086dcd361d200fbf6fcd1963653ad
hhman24 <anhoang483@gmail.com>
2023-07-06 22:51:57 +0700

    #5 hhman - Ux/UI main screen

---
+++ p1-encrypt-decrypt-app/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1_encrypt_decrypt_app.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ p1-encrypt-decrypt-app/ViewModels/ViewModelCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace p1_encrypt_decrypt_app.ViewModels
+{
+    class ViewModelCommand : ICommand
+    {
+
+        // Fields
+        private readonly Action<object> _executeAction;
+        private readonly Predicate<object> _canExecuteAction;
+
+        // Constructor
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = null;
+        }
+
+        public ViewModelCommand(Action<object> executeAction, Predicate<object> canExecuteAction)
+        {
+            if (executeAction == null)
+                throw new ArgumentNullException("execute");
+
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        // Events
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        // Methods
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecuteAction == null ? true : _canExecuteAction(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
